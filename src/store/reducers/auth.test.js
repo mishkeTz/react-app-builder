fd0fd7fc4c0d96c2ba4cd690f81398f02213813c
Nikola Misic <mishke.deejay@gmail.com>
2019-03-17 20:18:29 +0100

    Added test for reducer

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,33 @@
+import reducer from './auth.js';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+
+    let initialState;
+
+    beforeEach(() => {
+        initialState = {
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/',
+        };
+    })
+
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should store token upon login', () => {
+        expect(reducer(initialState, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-user-id',
+        })).toEqual({
+            ...initialState,
+            token: 'some-token',
+            userId: 'some-user-id',
+        });
+    })
+});
